f82892ff1542cffdfe8828394c626023a0bdb8a0
Yaezaki-YZK <quang.huy.hpqh@gmail.com>
2024-01-07 19:58:51 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Player from "./components/page/Player.js";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Player />
+      </header>
+    </div>
+  );
+}
+
+export default App;
