ff8c11eaea294dbb07d4cb3b0866a3fc0c8b1201
Yaezaki-YZK <quang.huy.hpqh@gmail.com>
2024-01-14 02:50:48 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Player from "./components/page/Player.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Player />
-      </header>
+      <Player />
     </div>
   );
 }
